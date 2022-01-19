be35fffedd4ab2d1b06e03f1cdbcb02e2d751060
erikaesthetic <erik@aesthetic.am>
2022-01-19 14:50:35 +0400

    test

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Swiss Theme',
+    'description': 'A description for your theme.',
+    'version': '1.0',
+    'author': 'Erik Saryan',
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+    ],
+}
