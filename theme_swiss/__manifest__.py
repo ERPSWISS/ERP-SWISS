4f3e54751bde7fda207b01cf44eee73c82f9c058
erikaesthetic <erik@aesthetic.am>
2022-01-20 17:38:11 +0400

    Add theme and some xml files to test that theme

--- theme_swiss/__manifest__.py
+++ theme_swiss/__manifest__.py
@@ -1,13 +1,21 @@
+# -*- encoding: utf-8 -*-
+
 {
-    'name': 'Swiss Theme',
-    'description': 'Theme for Swiss project',
-    'version': '1.0',
-    'author': 'Erik Saryan',
-    'category': 'Theme/Creative',
+    'name': 'Aesthetic',
+    'description': 'Aesthetic website theme',
+    'category': 'Theme/Services',
+    'summary': 'Corporate, Business, Tech, Services',
+    'sequence': 150,
+    'version': '1.0.0',
+    'author': 'eSwap',
     'depends': ['website'],
     'data': [
-        'views/layout.xml'
+        "views/layout.xml",
+        "views/pages.xml",
     ],
-    'application' : True,
+    'images': [
+        'static/description/cover.png',
+        'static/description/theme_default_screenshot.jpg',
+    ],
     'license': 'LGPL-3',
 }
